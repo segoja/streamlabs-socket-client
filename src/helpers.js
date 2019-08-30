1de409dd95760358eef93231d10ad3a78c150ec2
Aleš Kalan <SloRunner@users.noreply.github.com>
2019-08-30 22:16:05 +0200

    Update helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,3 @@
-export const removeCommas = str => typeof str == 'string' ? Number(str.replace(/,/g, '')) : str;
+export const removeCommas = str => typeof str === 'string' ? Number(str.replace(/,/g, '')) : str;
 
-export const removeNonNumeric = str => typeof str == 'string' ? Number(str.replace(/[^0-9.]/g, '')) : str;
+export const removeNonNumeric = str => typeof str === 'string' ? Number(str.replace(/[^0-9.]/g, '')) : str;
